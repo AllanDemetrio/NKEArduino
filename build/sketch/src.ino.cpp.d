1659ef7580ee8ffe3d4c67d37652159bdac42cbf
AllanRoxcode <allan.demetrio@roxcode.dev>
2024-03-27 22:42:41 -0300

    with build folder

---
+++ build/sketch/src.ino.cpp.d
@@ -0,0 +1,13 @@
+/Users/saintaln/uergs/projetoEmbarcado/NKEArduino/build/sketch/src.ino.cpp.o: \
+ /Users/saintaln/uergs/projetoEmbarcado/NKEArduino/build/sketch/src.ino.cpp \
+ /Users/saintaln/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/Arduino.h \
+ /Users/saintaln/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/binary.h \
+ /Users/saintaln/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/WCharacter.h \
+ /Users/saintaln/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/WString.h \
+ /Users/saintaln/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/HardwareSerial.h \
+ /Users/saintaln/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/Stream.h \
+ /Users/saintaln/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/Print.h \
+ /Users/saintaln/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/Printable.h \
+ /Users/saintaln/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/USBAPI.h \
+ /Users/saintaln/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/Arduino.h \
+ /Users/saintaln/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/variants/standard/pins_arduino.h
